a55ba23b023103c0a963870dd5a07700ba8ebf49
v1ab1 <sharkandmouse@gmail.com>
2022-10-21 01:33:17 +0500

    predone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.sass';
+import './index.css';
+import './fonts.css';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 
