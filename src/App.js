eb8eae9824fe263ac57e00a154e145386cf6df9f
v1ab1 <sharkandmouse@gmail.com>
2022-11-08 21:09:37 +0500

    react router cutted, loading fixed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import Filters from './components/Filters';
 import axios from 'axios';
 import {useEffect,useState} from 'react';
 import spin from './svg/spin.gif';
-import {Route, Routes} from "react-router-dom";
 
 function App() {
   let [items, setItems] = useState([]);
-  let [items2, setItems2] = useState([]);
-  let [items3, setItems3] = useState([]);
   const [bagItems, setBagItems] = useState([]);
   const [bagOpened, setBagOpened] = useState(false);
   const [handleBuying, setHandleBuying] = useState(true);
@@ -19,22 +16,13 @@ function App() {
     axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items').then((res) => {
       setItems(res.data);
     });
-    axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items2').then((res) => {
-      setItems2(res.data);
-    });
-    axios.get('https://635401c1ccce2f8c02013cf9.mockapi.io/items3').then((res) => {
-      setItems3(res.data);
-    });
-    axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/bag').then((res) => {
-      setBagItems(res.data);
-    });
   }, []);
 
   const onBuyButton = (obj) => {
     try {
       axios.post('https://634f201e4af5fdff3a6ee8b5.mockapi.io/bag', obj);
       setBagItems([...bagItems, obj]);
-    } catch (error) {
+    } catch (Error) {
       alert('Try again'); 
     }
   };
@@ -42,96 +30,45 @@ function App() {
   const onRemoveItem = (id) => {
     try {
       axios.delete(`https://634f201e4af5fdff3a6ee8b5.mockapi.io/bag/${id}`);
-      setBagItems((prev) => prev.filter(item => item.id != id));
-    } catch (error) {
+      setBagItems((prev) => prev.filter(item => item.id !== id));
+    } catch (Error) {
       alert('Try again');
     }
   };
 
   const upperCost = () => {
     items = [...items.sort((a, b) => a.price < b.price ? 1 : -1)];
-    items2 = [...items2.sort((a, b) => a.price < b.price ? 1 : -1)];
-    items3 = [...items3.sort((a, b) => a.price < b.price ? 1 : -1)];
     setItems(items);
-    setItems2(items2);
-    setItems3(items3);
   };
   const downCost = () => {
     items = [...items.sort((a, b) => a.price > b.price ? 1 : -1)];
-    items2 = [...items2.sort((a, b) => a.price > b.price ? 1 : -1)];
-    items3 = [...items3.sort((a, b) => a.price > b.price ? 1 : -1)];
     setItems(items);
-    setItems2(items2);
-    setItems3(items3);
   };
   const vlablChoice = () => {
     axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items').then((res) => {
       setItems(res.data);
     });
-    axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items2').then((res) => {
-      setItems2(res.data);
-    });
-    axios.get('https://635401c1ccce2f8c02013cf9.mockapi.io/items3').then((res) => {
-      setItems3(res.data);
-    });
   };
 
   return (
     <div className="wrapper">
-      <Header onClickBag={() => setBagOpened(!bagOpened)} />
+      <Header items={items} setItems={setItems} onClickBag={() => setBagOpened(!bagOpened)} />
       <div className="content">
         {bagOpened && <Bag bagItems={bagItems} onRemove={onRemoveItem} handleBuy={handleBuying} setBuy={setHandleBuying} setOpen={setBagOpened} open={bagOpened} />}
         <Filters upper={upperCost} down={downCost} vlabl={vlablChoice}/>
         <div className="items-wrapper">
           {items.length === 0 ? <img className='spin' src={spin} alt="" /> : null}
           <div className="items">
-          <Routes>
-            <Route path="/" element={
-              <>
-                {items.map((item, index) => (
-                  <Card 
-                    key={index}
-                    title={item.title}
-                    spec={item.spec}
-                    price={item.price}
-                    imageUrl={item.imageUrl}
-                    onBuy={onBuyButton}
-                  />
-                ))}
-              </>
-            }>
-            </Route>
-            <Route path="/desktop" element={
-              <>
-                {items2.map((item, index) => (
-                  <Card 
-                    key={index}
-                    title={item.title}
-                    spec={item.spec}
-                    price={item.price}
-                    imageUrl={item.imageUrl}
-                    onBuy={onBuyButton}
-                  />
-                ))}
-              </>
-            }>
-            </Route>
-            <Route path="/iphone" element={
-              <>
-                {items3.map((item, index) => (
-                  <Card 
-                    key={index}
-                    title={item.title}
-                    spec={item.spec}
-                    price={item.price}
-                    imageUrl={item.imageUrl}
-                    onBuy={onBuyButton}
-                  />
-                ))}
-              </>
-            }>
-            </Route>
-          </Routes>
+            {items.map((item, index) => (
+              <Card 
+                key={index}
+                title={item.title}
+                spec={item.spec}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onBuy={onBuyButton}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,42 @@
-import {Link} from "react-router-dom";
+import axios from 'axios';
 
-function Header(props) {
+function Header({onClickBag, items, setItems}) {
+    function reload(a) {
+        items = [];
+        setItems(items);
+        if (a === 1) {
+            axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items').then((res) => {
+                setItems(res.data);
+            });
+        }
+        if (a === 2) {
+            axios.get('https://634f201e4af5fdff3a6ee8b5.mockapi.io/items2').then((res) => {
+                setItems(res.data);
+            });
+        }
+        if (a === 3) {
+            axios.get('https://635401c1ccce2f8c02013cf9.mockapi.io/items3').then((res) => {
+                setItems(res.data);
+            });
+        }
+    }
+
     return (
         <header>
             <div className="header">
                 <div className="headerLeft">
                     <img src="./media/logo.svg" alt="" />
-                    <Link to="/">
-                        <span className="header-store">
-                            Macbook
-                        </span>
-                    </Link>
-                    <Link to="/desktop">
-                        <span className="header-store">
-                            Desktop
-                        </span>
-                    </Link>
-                    <Link to="/iPhone">
-                        <span className="header-store">
-                            iPhone
-                        </span>
-                    </Link>
+                    <span onClick={() => reload(1)} className="header-store">
+                        Macbook
+                    </span>
+                    <span onClick={() => reload(2)} className="header-store">
+                        Desktop
+                    </span>
+                    <span onClick={() => reload(3)} className="header-store">
+                        iPhone
+                    </span>
                 </div>
-                <div onClick={props.onClickBag} className="headerRight">     
+                <div onClick={onClickBag} className="headerRight">     
                     <svg height="44" viewBox="0 0 13 44" width="13" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path d="m0 44h13v-44h-13z"/><path d="m12 25.9c0 .6-.5 1.1-1.1 1.1h-8.8c-.6 0-1.1-.5-1.1-1.1v-7.9c0-.5.5-1 1.1-1h8.9c.6 0 1.1.5 1.1 1.1v7.8zm-5.5-11.9c1.2 0 2.1.9 2.3 2h-4.6c.2-1.1 1.1-2 2.3-2zm4.4 2h-1.1c-.2-1.7-1.6-3-3.3-3s-3.1 1.3-3.3 3h-1.1c-1.2 0-2.1.9-2.1 2.1v7.9c0 1.1.9 2 2.1 2h8.9c1.1 0 2.1-.9 2.1-2.1v-7.9c-.1-1.1-1-2-2.2-2z" fill="#fff"/></g>
                     </svg>
                 </div>
